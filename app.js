aefbcc4de3d0c4a98068944a28145adcae29c797
lorenzosalti <lorenzosalti@gmail.com>
2025-03-26 17:16:24 +0100

    feat: add /data/posts.js for better code reading

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const posts = require('./data/posts.js');
 const app = express();
 const port = 3000;
 
@@ -8,54 +9,24 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
 
-  console.log('Sto chiamando la Hompage del mio blog')
+  console.log('Sto chiamando la Hompage del mio blog');
 
   res.send('Server del mio blog');
 
-})
+});
+
 
 app.get('/bacheca', (req, res) => {
 
   console.log('Sto chiamando la Bacheca del mio blog');
 
-  const posts = [
-    {
-      titolo: 'Ciambellone',
-      contenuto: 'Ricetta: lorem ipsum dolor',
-      immagine: 'imgs/ciambellone.jpeg',
-      tags: ['#cucina', '#pasticceria', '#ciambellone', '#ricetta']
-    },
-    {
-      titolo: 'Cracker di Barbabietola',
-      contenuto: 'Ricetta: lorem ipsum dolor',
-      immagine: 'imgs/cracker_barbabietola.jpeg',
-      tags: ['#cucina', '#panificazione', '#cracker', '#barbabietola', '#ricetta']
-    },
-    {
-      titolo: 'Pane Fritto',
-      contenuto: 'Ricetta: lorem ipsum dolor',
-      immagine: 'imgs/pane_fritto_dolce.jpeg',
-      tags: ['#cucina', '#pasticceria', '#panefritto', '#ricetta']
-    },
-    {
-      titolo: 'Pasta di Barbabietola',
-      contenuto: 'Ricetta: lorem ipsum dolor',
-      immagine: 'imgs/pasta_barbabietola.jpeg',
-      tags: ['#cucina', '#pastafresca', '#barbabietola', '#ricetta']
-    },
-    {
-      titolo: 'Torta Paesana',
-      contenuto: 'Ricetta: lorem ipsum dolor',
-      immagine: 'imgs/torta_paesana.jpeg',
-      tags: ['#cucina', '#pasticceria', '#torta', '#tortapaesana', '#ricetta']
-    }
-  ];
-
   res.json(posts);
 
-})
+});
 
 
 app.listen(port, () => {
+
   console.log(`Server in ascolto alla porta ${port}`);
-})
+
+});
---
+++ data/posts.js
@@ -0,0 +1,34 @@
+const posts = [
+  {
+    titolo: 'Ciambellone',
+    contenuto: 'Ricetta: lorem ipsum dolor',
+    immagine: 'imgs/ciambellone.jpeg',
+    tags: ['#cucina', '#pasticceria', '#ciambellone', '#ricetta']
+  },
+  {
+    titolo: 'Cracker di Barbabietola',
+    contenuto: 'Ricetta: lorem ipsum dolor',
+    immagine: 'imgs/cracker_barbabietola.jpeg',
+    tags: ['#cucina', '#panificazione', '#cracker', '#barbabietola', '#ricetta']
+  },
+  {
+    titolo: 'Pane Fritto',
+    contenuto: 'Ricetta: lorem ipsum dolor',
+    immagine: 'imgs/pane_fritto_dolce.jpeg',
+    tags: ['#cucina', '#pasticceria', '#panefritto', '#ricetta']
+  },
+  {
+    titolo: 'Pasta di Barbabietola',
+    contenuto: 'Ricetta: lorem ipsum dolor',
+    immagine: 'imgs/pasta_barbabietola.jpeg',
+    tags: ['#cucina', '#pastafresca', '#barbabietola', '#ricetta']
+  },
+  {
+    titolo: 'Torta Paesana',
+    contenuto: 'Ricetta: lorem ipsum dolor',
+    immagine: 'imgs/torta_paesana.jpeg',
+    tags: ['#cucina', '#pasticceria', '#torta', '#tortapaesana', '#ricetta']
+  }
+];
+
+module.exports = posts;
