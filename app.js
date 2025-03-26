1787157c31a6f7b54915bbbdbc28316622491010
lorenzosalti <lorenzosalti@gmail.com>
2025-03-26 15:56:31 +0100

    feat: add listen on port 3000

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server in ascolto alla porta ${port}`);
+})
